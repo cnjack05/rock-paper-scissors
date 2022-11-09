5cc8fb8589caf4911972d6156cdbb77bdf22fe30
cnjack05 <117835844+cnjack05@users.noreply.github.com>
2022-11-09 09:33:38 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+let set_hand_to = 0
+input.onGesture(Gesture.Shake, function () {
+    set_hand_to = randint(1, 3)
+    if (("hand" as any) == ("1" as any)) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (("hand" as any) == ("2" as any)) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
